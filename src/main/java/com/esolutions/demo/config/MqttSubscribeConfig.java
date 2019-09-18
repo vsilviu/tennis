f241a84cd3ba22f5aad991494cccbceb175ec59f
student <student@student.com>
2019-09-18 14:31:52 +0300

    - added client command topic subscribe

--- src/main/java/com/esolutions/demo/config/MqttSubscribeConfig.java
+++ src/main/java/com/esolutions/demo/config/MqttSubscribeConfig.java
@@ -3,6 +3,7 @@ package com.esolutions.demo.config;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -11,16 +12,25 @@ import javax.annotation.PostConstruct;
 public class MqttSubscribeConfig {
 
     private final MqttClient mqttClient;
+    private final Environment environment;
 
     @Autowired
-    public MqttSubscribeConfig(MqttClient mqttClient) {
+    public MqttSubscribeConfig(MqttClient mqttClient, Environment environment) {
         this.mqttClient = mqttClient;
+        this.environment = environment;
     }
 
     @PostConstruct
     public void listen() throws MqttException {
-        mqttClient.subscribe("/match/connect", (topic, msg) -> {
-            System.out.println("A connection attempt was received from WS session id " + new String(msg.getPayload()));
+        mqttClient.subscribe("/match/connect/" + environment.getProperty("clientId"), (topic, msg) -> {
+            System.out.println(String.format("[%s] : I have connected with you, %s", msg, environment.getProperty("clientId")));
+        });
+
+        mqttClient.subscribe(String.format("/match/command/%s", environment.getProperty("clientId")), (topic, msg) -> {
+            String content = new String(msg.getPayload());
+            String action = content.split("#")[0];
+            String sessionId = content.split("#")[1];
+            System.out.println(String.format("[%s] : Execute the following command, %s", sessionId, action));
         });
     }
 }
